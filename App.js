46e110a6a6efed1790f94154ad0f4f62acae35de
Chan Yun Hong <yhchan@Chans-MacBook-Air.local>
2021-01-14 19:56:48 +0800

    add header with logo and navigation

--- App.js
+++ App.js
@@ -8,10 +8,11 @@
 
 import React from 'react';
 import 'react-native-gesture-handler';
-import {StatusBar} from 'react-native';
+import {Image, StatusBar, View} from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
 import DiscoveryScreen from './src/screens/DiscoveryScreen';
 import CreatePostScreen from './src/screens/CreatePostScreen';
 import NotificationsScreen from './src/screens/NotificationsScreen';
@@ -20,8 +21,45 @@ import Foundation from 'react-native-vector-icons/Foundation';
 import ADIcon from 'react-native-vector-icons/AntDesign';
 import Feather from 'react-native-vector-icons/Feather';
 import IoniconsIcon from 'react-native-vector-icons/Ionicons';
+import logo from './src/assets/images/logo.png';
 
 const Tab = createBottomTabNavigator();
+
+const HomeStack = createStackNavigator();
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'Instagram',
+          headerLeftContainerStyle: {
+            marginLeft: 15,
+          },
+          headerRightContainerStyle: {
+            marginRight: 15,
+          },
+          headerLeft: () => (
+            <View>
+              <Feather name="camera" size={25} color={'#000'} />
+            </View>
+          ),
+          headerTitle: () => (
+            <Image source={logo} style={{width: 125, resizeMode: 'contain'}} />
+          ),
+          headerRight: () => (
+            <IoniconsIcon
+              name="paper-plane-outline"
+              size={25}
+              color={'#545454'}
+            />
+          ),
+        }}
+      />
+    </HomeStack.Navigator>
+  );
+}
 
 const App: () => React$Node = () => {
   return (
@@ -61,7 +99,7 @@ const App: () => React$Node = () => {
             inactiveTintColor: 'gray',
             showLabel: false,
           }}>
-          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Home" component={HomeStackScreen} />
           <Tab.Screen name="Discovery" component={DiscoveryScreen} />
           <Tab.Screen name="Post" component={CreatePostScreen} />
           <Tab.Screen name="Notifications" component={NotificationsScreen} />
