e997f3cb00a90610460f43817587bd29c5e55f21
yhchan0918 <yhchan0918@gmail.com>
2021-01-15 22:37:16 +0800

    add amplify and authentication

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {StatusBar, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {withAuthenticator} from 'aws-amplify-react-native';
 import Router from './src/router';
 
 const App: () => React$Node = () => {
@@ -23,4 +24,4 @@ const App: () => React$Node = () => {
   );
 };
 
-export default App;
+export default withAuthenticator(App);
